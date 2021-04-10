1d6a4664171aebd5dbee110be7943e7887263c73
betancurdamian <da_ariel@hotmail.com>
2021-04-10 02:01:20 -0300

    por las dudas hago commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,51 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+        List<Integer> datoOriginal = new ArrayList<>();
+        datoOriginal.add(0);
+        datoOriginal.add(1);
+        datoOriginal.add(2);
+        datoOriginal.add(3);
+        datoOriginal.add(4);
+        datoOriginal.add(5);
+        datoOriginal.add(6);
+        datoOriginal.add(7);
+        datoOriginal.add(8);
+        datoOriginal.add(9);
+
+        int cantidad = tamanio(datoOriginal);
+
+        List<Integer> datoModificado = new ArrayList<>();
+
+
+        for(int i = 0, j = cantidad - 1; i < cantidad; i++, j--){
+            datoModificado.add(i,datoOriginal.get(j));
+
+        }
+
+
+
+        for(int x = cantidad ; x <= 0; x--){
+            datoModificado.add(cantidad-x, datoOriginal.get(x));
+
+        }
+
+        System.out.println("datos modificados");
+
+        for (Integer d : datoModificado) {
+            System.out.println(d);
+        }
+
+
+    }
+
+    public static int tamanio(List<Integer> dato){
+        return dato.size();
     }
 }
